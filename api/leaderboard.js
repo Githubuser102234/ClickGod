f4096dc62288ff2ea82c909e5bd0e0d818a4db0c
Githubuser102234 <119143251+Githubuser102234@users.noreply.github.com>
2025-09-21 19:36:24 -0400

    Update and rename api/leaderboard.js to leaderboard.js

--- api/leaderboard.js
+++
@@ -1,9 +0,0 @@
-// api/leaderboard.js
-const fs = require('fs');
-const path = require('path');
-
-const filePath = path.join(process.cwd(), 'leaderboard.json');
-
-// Helper function to read the leaderboard file
-function readLeaderboard() {
-    
